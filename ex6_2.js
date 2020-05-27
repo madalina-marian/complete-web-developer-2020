177e58c95f2560e1952ff57e488c0b72a9c33ece
Madalina <madalinamarian2@gmail.com>
2020-05-27 23:55:41 +0300

    array

---
+++ ex6_2.js
@@ -0,0 +1,12 @@
+/**
+* Created by Madalina Marian on 27/05/2020
+*/
+
+// you should have at the end:
+// ["Kiwi", "Oranges", "Blueberries"]
+
+// using this array,
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
+
+array2[1][1][0];
