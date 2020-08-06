1b4a7f55c48957c81197ce45f548e32abade377d
Madalina <madalinamarian2@gmail.com>
2020-08-06 12:59:36 +0300

    generate bg color function

--- math-objects.js
+++ math-objects.js
@@ -5,9 +5,6 @@
 
 /**
 a) Write a JavaScript function to generate a random integer. Function will take two parameters "min" and "max".
-
-b) Create random CSS background color code. The code could be HEX or RGB.
-Call that function and apply it to a container of an HTML page.
 **/
 
 var generateRandomInteger = function(min,max) {
@@ -17,6 +14,30 @@ var generateRandomInteger = function(min,max) {
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 };
 
-console.log(generateRandomInteger(1, 2));
-console.log(generateRandomInteger(5, 8));
-console.log(generateRandomInteger(50, 110));
+// console.log(generateRandomInteger(1, 2));
+// console.log(generateRandomInteger(5, 8));
+// console.log(generateRandomInteger(50, 110));
+
+
+/**
+b) Create random CSS background color code. The code could be HEX or RGB.
+Call that function and apply it to a container of an HTML page.
+**/
+
+function get_random_rgb_color_code() {
+    var x = generate_random_rgb_number();
+    var y = generate_random_rgb_number();
+    var z = generate_random_rgb_number();
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+    return bgColor;
+}
+
+function generate_random_rgb_number() {
+    return Math.floor(Math.random() * 256);
+}
+document.body.style.background = get_random_rgb_color_code();
+
+console.log(Math.random());
+
+
